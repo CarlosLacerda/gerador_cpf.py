7ca2c4b2ae3b6c6bf9f5a93221ce371a13245def
CarlosLacerda <carloslacerda684@gmail.com>
2026-03-23 18:33:27 -0300

    adiciona gerador de CPFs válidos com cálculo dos dígitos verificadores

---
+++ aula67.py
@@ -0,0 +1,36 @@
+# Aula 64 do professor - 104 UDEMY
+
+import random
+import sys
+
+for _ in range(100):
+    nove_digitos = ''
+    for i in range(9):
+        nove_digitos += str(random.randint(0, 9))
+
+    contador_regressivo_1 = 10
+
+    resultado_digito_1 = 0
+    for numero in nove_digitos:
+        resultado_digito_1 += int(numero) * contador_regressivo_1
+        contador_regressivo_1 -= 1
+    # print(resultado_digito_1)
+    # print(resultado_digito_1 * 10)
+    digito_1 = ((resultado_digito_1 * 10) % 11)
+    digito_1 = digito_1 if digito_1 <= 9 else 0
+    # print(digito_1)
+
+    dez_digitos = nove_digitos + str(digito_1)
+    contador_regressivo_2 = 11
+
+    resultado_digito_2 = 0
+    for numero in dez_digitos:
+        resultado_digito_2 += int(numero) * contador_regressivo_2
+        contador_regressivo_2 -= 1
+    digito_2 = (resultado_digito_2 * 10) % 11
+    digito_2 = digito_2 if digito_2 <= 9 else 0
+    # print(digito_2)
+
+    cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+    print(cpf_gerado_pelo_calculo)
